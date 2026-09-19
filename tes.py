085909d63952fa907267258e5c75cbe578221830
muhammadfadilla58-star <muhammadfadillla58@gmail.com>
2026-09-19 12:08:09 +0800

    Postest2

--- tes.py
+++
@@ -1,9 +0,0 @@
-angka = 2005.5432
-format_str = f"desimal = {angka:.2f}"
-print(format_str)
-angka = 2005.5432
-format_str = f"desimal = {angka:3.2f}"
-print(format_str)
-angka = 0.045
-format_persen = f"persentase = {angka:+.1%}"
-print(format_persen)
