2e2aa400abf977499820ffcc85d89db3274f2f8c
Muhammad Nur Fadilla <muhammadfadilla58@gmail.com>
2026-09-22 13:34:54 +0800

    Add files via upload

--- Post-test/Postest-apd-2/2609106036-MuhammadNurFadilla-PT-2.py
+++ Post-test/Postest-apd-2/2609106036-MuhammadNurFadilla-PT-2.py
@@ -3,15 +3,16 @@ makanan_2 = 16000
 makanan_3 = 19000
 makanan_4 = 20000
 makanan_5 = 21000
+makanan_6 = 22000
+biaya_aplikasi = 5000
 nim = 36
-harga_makanan = [makanan_1, makanan_2, makanan_3, makanan_4, makanan_5]
-total_harga = makanan_1 + makanan_2 + makanan_3 + makanan_4 + makanan_5
-mata_uang_euro = total_harga / 20422
-rata_rata = total_harga / len(harga_makanan)
+harga_makanan = [makanan_1, makanan_2, makanan_3, makanan_4, makanan_5, makanan_6]
+total_bayar = makanan_1 + makanan_2 + makanan_3 + makanan_4 + makanan_5 + makanan_6 + biaya_aplikasi
+mata_uang_euro = total_bayar / 20527
+rata_rata = total_bayar / len(harga_makanan)
 bolean = nim != rata_rata
-print(harga_makanan[-5:])
-print(f"Total harga makanan: Rp.{total_harga}")
+print(harga_makanan[-6:])
+print(f"Total bayar: Rp.{total_bayar}")
 print(f"Mata uang euro:  {mata_uang_euro:.2f} EUR")
-print(f"Rata-rata harga makanan: Rp. {int(rata_rata)}")   
+print(f"Rata-rata harga makanan: Rp. {rata_rata:.2f}")   
 print(bolean)
-
